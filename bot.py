118dfd0f747e4f2632948d80e0e59d7133ce5ee1
mendogoff <mendogoff@gmail.com>
2024-09-26 04:18:57 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ async def error_handler(update: object, context: CallbackContext):
     logger.error(f'Произошла ошибка: {context.error}')
 
 # Обработка вебхуков через Flask
-@app.route('/webhook/', methods=['POST'])
+await @app.route('/webhook/', methods=['POST'])
 def webhook():
     if request.method == "POST":
         update = Update.de_json(request.get_json(), application.bot)
@@ -66,3 +66,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
