2eec3fec05d96fda0bf2901c32a033030ba1642a
mendogoff <mendogoff@gmail.com>
2024-09-26 05:10:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ import asyncio
 logging.basicConfig(level=logging.INFO)
 
 # Получаем токен бота и ID канала из переменных окружения
-TOKEN = os.getenv("8027225541:AAFUq7VeX9zWHHfnISipKM53nZgi4s7_odg")
-CHANNEL_ID = os.getenv("-1002388187883")  # Убедитесь, что здесь указан правильный ID вашего канала
-WEBHOOK_URL = "https://https://api.render.com/deploy/srv-crq8k7l2ng1s73e46h40?key=rdyLgUfDFm4/webhook/"  # Ваш публичный URL для вебхука
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # Убедитесь, что в вашем окружении есть переменная TELEGRAM_BOT_TOKEN
+CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")  # Убедитесь, что в вашем окружении есть переменная TELEGRAM_CHANNEL_ID
+WEBHOOK_URL = "https://api.render.com/deploy/srv-crq8k7l2ng1s73e46h40?key=rdyLgUfDFm4/webhook/"  # Ваш публичный URL для вебхука
 
 # Инициализация Flask приложения
 app = Flask(__name__)
@@ -22,15 +22,11 @@ application = Application.builder().token(TOKEN).build()
 
 # Команда /start
 async def start(update: Update, context):
-    user_id = update.message.from_user.id
     await update.message.reply_text("Привет! Вы можете отправлять анонимные сообщения, которые будут пересылаться в канал.")
 
 # Обработчик текстовых сообщений - пересылка сообщений в канал
 async def handle_message(update: Update, context):
-    user_name = update.message.from_user.first_name
     text = update.message.text
-    
-    # Формирование анонимного сообщения
     message_to_send = f"Сообщение от анонима: {text}"
     
     # Отправляем сообщение в канал
@@ -55,7 +51,6 @@ async def set_webhook():
 # Обработчик вебхуков
 @app.route('/webhook/', methods=['POST'])
 async def webhook():
-    """Получение обновлений от Telegram через вебхук."""
     update = Update.de_json(request.get_json(), bot)
     await application.process_update(update)
     return "ok", 200
